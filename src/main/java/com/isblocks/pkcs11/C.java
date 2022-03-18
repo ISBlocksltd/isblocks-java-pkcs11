5280c49e0fedcafb224b4dc0800fd9388d54cfec
hemlatapednekar21 <hemlata.pednekar@isblocks.com>
2022-03-18 16:28:58 +0530

    Added file headers

--- src/main/java/com/isblocks/pkcs11/C.java
+++ src/main/java/com/isblocks/pkcs11/C.java
@@ -18,6 +18,45 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+/*/*************************************************************************
+
+ *  Copyright 2021 IS Blocks, Ltd. and/or its affiliates 				 *
+
+ *  and other contributors as indicated by the @author tags.	         *
+
+ *																		 *
+
+ *  All rights reserved													 *
+
+ * 																		 *
+
+ *  The use of this Proprietary Software are subject to specific         *
+
+ *  commercial license terms											 *
+
+ * 																		 *
+
+ *  To purchase a licence agreement for any use of this code please 	 *
+
+ *  contact info@isblocks.com 											 *
+
+ *																		 *
+
+ *  Unless required by applicable law or agreed to in writing, software  *
+
+ *  distributed under the License is distributed on an "AS IS" BASIS,    *
+
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      *
+
+ *  implied.															 *
+
+ *  See the License for the specific language governing permissions and  *
+
+ *  limitations under the License.                                       *
+
+ *                                                                       *
+
+ *************************************************************************/
 
 package com.isblocks.pkcs11;
 
--- src/main/java/com/isblocks/pkcs11/jna/JNA.java
+++ src/main/java/com/isblocks/pkcs11/jna/JNA.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2010-2011 Joel Hockey (joel.hockey@gmail.com). All rights reserved.
+
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -18,7 +19,45 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+/*************************************************************************
 
+ *  Copyright 2021 IS Blocks, Ltd. and/or its affiliates 				 *
+
+ *  and other contributors as indicated by the @author tags.	         *
+
+ *																		 *
+
+ *  All rights reserved													 *
+
+ * 																		 *
+
+ *  The use of this Proprietary Software are subject to specific         *
+
+ *  commercial license terms											 *
+
+ * 																		 *
+
+ *  To purchase a licence agreement for any use of this code please 	 *
+
+ *  contact info@isblocks.com 											 *
+
+ *																		 *
+
+ *  Unless required by applicable law or agreed to in writing, software  *
+
+ *  distributed under the License is distributed on an "AS IS" BASIS,    *
+
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      *
+
+ *  implied.															 *
+
+ *  See the License for the specific language governing permissions and  *
+
+ *  limitations under the License.                                       *
+
+ *                                                                       *
+
+ *************************************************************************/
 package com.isblocks.pkcs11.jna;
 
 import com.isblocks.pkcs11.C;
